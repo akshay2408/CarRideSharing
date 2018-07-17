6b54ae51348f231c213655a7a843ce4d80f18aa3
Akshay <akshaybabar2408@gmail.com>
2018-07-17 11:47:14 +0530

    Remove unwanted comments

--- BackEnd/apis/permissions.py
+++ BackEnd/apis/permissions.py
@@ -1,13 +1,1 @@
 # from rest_framework import permissions
-
-
-# class IsClientOrDriver(permissions.BasePermission):
-#     """
-#     Custom permission so users/agents can only view their own user data.
-#     ZigWay staff can see any user info
-#     """
-#     def has_permission(self, request, view):
-#         return request.user.is_staff or hasattr(request.user, 'agent')
-
-#     def has_object_permission(self, request, view, obj):
-#         return (obj == request.user) or request.user.is_staff
